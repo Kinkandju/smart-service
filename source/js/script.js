32419e8d53cc4d0e51cc716c276bc1f369eb64a8
Kinkandju <ferretsphilosophy@gmail.com>
2020-10-01 14:35:45 +0300

    Добавляет базовую стилизацию подвала сайта мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,45 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var navButton = document.querySelector('.navigation__button');
+var locationButton = document.querySelector('.location__button');
+var navList = document.querySelector('.navigation__list');
+var locationGroup = document.querySelector('.location__group');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navList.classList.remove('navigation__list--nojs');
+locationGroup.classList.remove('location__group--nojs');
+
+navButton.addEventListener('click', function () {
+  if (navList.classList.contains('navigation__list--closed') || locationGroup.classList.contains('location__group--opened')) {
+    navList.classList.remove('navigation__list--closed');
+    navList.classList.add('navigation__list--opened');
+    locationGroup.classList.remove('location__group--opened');
+    locationGroup.classList.add('location__group--closed');
+    navButton.classList.remove('navigation__button--closed');
+    navButton.classList.add('navigation__button--opened');
+    locationButton.classList.remove('location__button--closed');
+    locationButton.classList.add('location__button--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navList.classList.add('navigation__list--closed');
+    navList.classList.remove('navigation__list--opened');
+    navButton.classList.add('navigation__button--closed');
+    navButton.classList.remove('navigation__button--opened');
+  }
+});
+
+locationButton.addEventListener('click', function () {
+  if (locationGroup.classList.contains('location__group--closed') || navList.classList.contains('navigation__list--opened')) {
+    locationGroup.classList.remove('location__group--closed');
+    locationGroup.classList.add('location__group--opened');
+    navList.classList.remove('navigation__list--opened');
+    navList.classList.add('navigation__list--closed');
+    locationButton.classList.remove('location__button--closed');
+    locationButton.classList.add('location__button--opened');
+  } else {
+    locationGroup.classList.add('location__group--closed');
+    locationGroup.classList.remove('location__group--opened');
+    locationButton.classList.add('location__button--closed');
+    locationButton.classList.remove('location__button--opened');
+    navButton.classList.remove('navigation__button--closed');
+    navButton.classList.add('navigation__button--opened');
   }
 });
